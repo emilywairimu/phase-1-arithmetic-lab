55b1d24339eaf46a2c988f3ba083fb8200a8f279
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-03-14 05:55:49 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 25;
+let num4 = 7;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);  
+let mod = num3 % num4;
+console.log(mod);
+let max = Math.max(1, 5, 10, 20, 3);
+console.log(max);
